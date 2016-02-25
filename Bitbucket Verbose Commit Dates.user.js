dd3c502f2e06a1b6c680f412a2b27b7d81e9a6c6
Gifford Nowland <gnowland@gmail.com>
2016-02-25 12:57:47 -0600

    init Bitbucket Verbose Commit Dates

---
+++ Bitbucket Verbose Commit Dates.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Bitbucket Verbose Commit Dates
+// @namespace    http://bitbucket.org/
+// @version      0.1
+// @description  Display Verbose Dates on Commits page of Bitbucket.org
+// @author       Gifford Nowland
+// @match        bitbucket.org/*/commits/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+$(document).ready(function() {
+    $(".commit-list .date > div").css("width","auto");
+    $(".commit-list th.date").css("cursor","pointer");
+});
+
+$(".commit-list th.date").click(function(){
+    verboseCommit();
+});
+
+
+function verboseCommit(){
+    var $commitTime = $(".commit-list .date time");
+    
+    $commitTime.each(function() {
+        $(this).text($(this).attr("title"));
+    });
+}
