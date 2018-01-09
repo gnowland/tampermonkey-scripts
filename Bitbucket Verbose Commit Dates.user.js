b18f46322b46ab25c5df6a1f1c1272d6f8505b18
Gifford Nowland <hi@giffordnowland.com>
2018-01-09 14:25:18 -0800

    handle change

--- Bitbucket Verbose Commit Dates.user.js
+++ Bitbucket Verbose Commit Dates.user.js
@@ -12,12 +12,15 @@
 (function( $ ) {
     'use strict';
 
+    var itsShowTime = false;
+
     // Show commit time
     function showTime(){
         var $commitTime = $(".commit-list .date time");
         $commitTime.each(function() {
             $(this).text($(this).attr("title"));
         });
+        var itsShowTime = true;
     }
 
     // Hide bitbucket avatar
@@ -31,13 +34,20 @@
 
     // Page load
     $(document).ready(function() {
-        // Setup buttons
+        // Add boxes & styles
         $(".commit-list .date > div").css("width","auto");
         $(".app-header--secondary").append('<button id="showtime">Show Time</button>');
         $(".app-header--secondary").append('<button id="hideavatar">Hide Avatar</button>');
 
-        // Handle click
+        // Fire click event
         $("#showtime").click(function(){ showTime(); });
         $("#hideavatar").click(function(){ hideAvatar(); });
+
+        // Handle change
+        $( ".commit-list .date time" ).change(function() {
+            if (itsShowTime === true) {
+                showTime();
+            }
+        });
     });
 })(jQuery);
